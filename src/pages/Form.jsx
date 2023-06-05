71f4d9faea229649306dc1e9e112f568e62d5e73
shaqdeff <shaquillendunda@gmail.com>
2023-06-05 22:16:55 +0300

    Animate change of state

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 // components
 import { Login, SignUp, ToggleSwitch } from '../components';
@@ -6,19 +7,49 @@ import { Login, SignUp, ToggleSwitch } from '../components';
 const Form = () => {
   const [isLogin, setIsLogin] = useState(true);
 
+  const variants = {
+    initial: {
+      opacity: 0.1,
+      x: '-10%',
+    },
+    enter: {
+      opacity: 1,
+      x: '0%',
+    },
+    exit: {
+      opacity: 0.1,
+      x: '5%',
+    },
+  };
+
   return (
-    <div
-      className="h-screen w-screen flex
-    justify-center items-center">
-      <div
-        className="m-auto border rounded-[30px]
-      flex flex-col justify-center
-      gap-10 items-center w-[35%] relative">
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="m-auto border rounded-[30px] flex flex-col justify-center gap-10 items-center w-[35%] relative">
         <div className="mt-8">
           <ToggleSwitch setIsLogin={setIsLogin} isLogin={isLogin} />
         </div>
 
-        {isLogin ? <Login /> : <SignUp />}
+        {isLogin ? (
+          <motion.div
+            className="w-full overflow-hidden"
+            key="login"
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            variants={variants}>
+            <Login />
+          </motion.div>
+        ) : (
+          <motion.div
+            className="w-full overflow-hidden"
+            key="signup"
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            variants={variants}>
+            <SignUp />
+          </motion.div>
+        )}
       </div>
     </div>
   );
