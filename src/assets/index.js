3e1075a1b31ad4469854c0d1fcb7ab6f3242aa15
shaqdeff <shaquillendunda@gmail.com>
2023-08-02 11:04:14 +0300

    Remove duplicate files

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import bgImage from './images/bg.webp';
-import gmail from './icons/gmail.png';
-
-export { bgImage, gmail };
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Login from './Login';
-import SignUp from './SignUp';
-import ToggleSwitch from './ToggleSwitch';
-
-export { Login, SignUp, ToggleSwitch };
